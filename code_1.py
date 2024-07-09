3802f51f803e22cc570a14284d180308afa74fee
Pavan6614 <pavanchinna6614@gmail.com>
2024-07-09 15:46:27 +0530

    Add files via upload

---
+++ code_1.py
@@ -0,0 +1,10 @@
+#List create,delete,append,reverse
+list=eval(input("Enter List:"))   
+print(list)
+list.append(7)
+print(list)
+list.remove(2)
+list.reverse()
+print(list) 
+
+#Tuples create,delete,append,reverse
